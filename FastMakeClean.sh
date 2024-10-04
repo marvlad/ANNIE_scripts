a912b2a453203513a1189ed7a5259d8102dd0263
Marvin V. Ascencio S. <marvlad@users.noreply.github.com>
2024-10-04 09:09:35 -0500

    Create FastMakeClean.sh
    make clean fast

---
+++ FastMakeClean.sh
@@ -0,0 +1,298 @@
+Last login: Fri Oct  4 01:06:00 on ttys007
+➜  ~ annie 1
+You have an active ticket
+                              NOTICE TO USERS
+
+       This  is a Federal computer (and/or it is directly connected to a
+       Fermilab local network system) that is the property of the United
+       States Government.  It is for authorized use only.  Users (autho-
+       rized or unauthorized) have no explicit or  implicit  expectation
+       of privacy.
+
+       Any  or  all uses of this system and all files on this system may
+       be intercepted, monitored, recorded,  copied, audited, inspected,
+       and  disclosed  to authorized site, Department of Energy  and law
+       enforcement personnel, as  well as authorized officials of  other
+       agencies,  both  domestic and foreign.  By using this system, the
+       user consents to such interception, monitoring, recording,  copy-
+       ing,  auditing,  inspection,  and disclosure at the discretion of
+       authorized site or Department of Energy personnel.
+
+       Unauthorized or improper use of this system may result in  admin-
+       istrative  disciplinary  action and civil and criminal penalties.
+       By continuing to use this system you indicate your  awareness  of
+       and  consent to these terms and conditions of use.  LOG OFF IMME-
+       DIATELY if you do not agree to  the  conditions  stated  in  this
+       warning.
+
+       Fermilab  policy  and  rules for computing, including appropriate
+       use, may be found at http://www.fnal.gov/cd/main/cpolicy.html
+Last login: Mon Sep 30 16:58:40 2024 from 192.190.216.109
+------------------------------------------------------------------------------
+                     ..::Powered by SCF-SSI::..                      
+
+   Hostname: anniegpvm01.fnal.gov        OS Release: Alma Linux 9.4            
+         IP: 131.225.67.195                  Subnet: 255.255.255.0             
+
+     Kernel: 5.14.0-427.20.1                   Arch: x86_64                    
+        RAM: 11.43 GiB                         Swap: 2.00 GiB                  
+      Cores: 4                              Virtual: kvm                       
+
+ SSH Logins: 2                             Load Avg: 0.08 0.61 2.4             
+
+       Help: https://ssiwiki.fnal.gov/wiki/Interactive_Server_Facility         
+------------------------------------------------------------------------------
+[mascenci@anniegpvm01 ~]$ app
+-bash: app: command not found
+[mascenci@anniegpvm01 ~]$ annie
+[mascenci@anniegpvm01 ~]$ ls -lrt
+total 244
+-rw-r--r--+ 1 mascenci nobody 4965 Aug 26  2017 setup_dune.sh
+-rw-r--r--+ 1 mascenci nobody 3617 Oct 25  2017 minerva101setup.sh
+-rw-r--r--+ 1 mascenci nobody  215 May 23  2018 setup_fix_v21r1p1.sh
+-rwxr-xr-x+ 1 mascenci nobody  330 May 23  2018 setup_fix_v10r8p12.sh
+-rw-r--r--+ 1 mascenci nobody   68 Jun 23  2018 setup_d.sh
+-rw-------+ 1 mascenci nobody 7862 Aug 17  2018 oeXauthority
+-rwxr-xr-x+ 1 mascenci nobody  330 Aug 20  2018 setup_fix_v10r8p11.sh
+-rw-r--r--+ 1 mascenci nobody  241 Aug 27  2018 setup_v21r1p1_NuE.sh
+-rw-r--r--+ 1 mascenci nobody  242 Oct  1  2018 setup_v21r1p1_NuMu.sh
+-rw-r--r--+ 1 mascenci nobody  100 Jun  4  2019 genie_setup.sh
+-rw-r--r--+ 1 mascenci nobody  688 Jul  3  2019 SETUP.sh
+-rw-r--r--+ 1 mascenci nobody  776 Jul  4  2019 setup_all.sh
+-rw-r--r--+ 1 mascenci nobody  124 Aug 20  2019 ntuple-server-dst.log
+-rw-r--r--+ 1 mascenci nobody  149 Sep 27  2019 setup_v21r1p1_MEC.sh
+-rw-r--r--+ 1 mascenci nobody  151 Sep 27  2019 setup_v21r1p1_CCQENu.sh
+-rw-r--r--+ 1 mascenci nobody  157 Sep 27  2019 setup_v21r1p1_MasterAnaDev.sh
+-rw-------+ 1 mascenci nobody 7173 Oct 15  2019 diego_setup_v21r1p1.sh
+-rw-------+ 1 mascenci nobody  220 Oct 22  2019 Unfold.sh
+-rw-------+ 1 mascenci nobody  199 Oct 28  2019 SentJobs.sh
+-rw-------+ 1 mascenci nobody  114 Nov  4  2019 MEC_jobs.sh
+-rw-r--r--+ 1 mascenci nobody  152 Jan 29  2020 grid_macro.sh
+-rw-------+ 1 mascenci nobody  209 Jul 15  2020 NUnfolfing.sh
+-rw-r--r--+ 1 mascenci nobody 4381 Jul 22  2020 Huma.sh
+-rw-------+ 1 mascenci nobody  220 Aug 17  2020 TUnfolding.sh
+-rw-------+ 1 mascenci nobody  229 Aug 25  2020 UnfoldS7.sh
+-rw-r--r--+ 1 mascenci nobody  164 Sep 12  2020 setup_v21r1p1.sh
+-rw-r--r--+ 1 mascenci nobody  163 Nov  6  2020 S7setup_v22r1p1.sh
+-rw-r--r--+ 1 mascenci nobody  166 Dec  9  2020 NewMECSL7.sh
+-rw-r--r--+ 1 mascenci nobody  164 Dec 18  2020 diego_v22r1p1.sh
+-rw-r--r--+ 1 mascenci nobody  255 Dec 26  2020 GiBUU_setup.sh
+-rw-r--r--+ 1 mascenci nobody  270 Dec 26  2020 NuWro_setup.sh
+-rw-r--r--+ 1 mascenci nobody  159 Dec 26  2020 cmtSetup.sh
+-rw-r--r--+ 1 mascenci nobody  413 Jan  2  2021 Neut_setup.sh
+-rw-r--r--+ 1 mascenci nobody  253 Jan 11  2021 setupFaiza.sh
+-rw-r--r--+ 1 mascenci nobody  268 Feb 11  2021 setup_wc.sh
+-rw-r--r--+ 1 mascenci nobody  152 Mar 19  2021 grid_macroS7.sh
+-rw-r--r--+ 1 mascenci nobody 1239 Sep 14  2021 ssh_mnv.sh
+-rw-r--r--+ 1 mascenci nobody  237 Oct 19  2021 annie_setup.sh
+-rw-r--r--+ 1 mascenci nobody  621 Dec  3  2021 singularityTestSL7.sh
+-rw-r--r--+ 1 mascenci nobody  381 Dec  8  2021 ANNIE_singularity.sh
+-rw-r--r--+ 1 mascenci nobody  420 Jan 18  2022 otromux.txt
+drwxr-sr-x+ 3 mascenci nobody 2048 Feb 10  2022 usr
+drwxr-sr-x+ 2 mascenci nobody 2048 Feb 12  2022 perl5
+drwxr-sr-x+ 3 mascenci nobody 2048 Feb 15  2022 cmtuser
+drwxr-sr-x+ 2 mascenci nobody 2048 May 28  2022 myTestTerminal
+drwxr-sr-x+ 6 mascenci nobody 2048 May 28  2022 powerlevel10k
+-rw-r--r--+ 1 mascenci nobody  644 Jul  5  2022 runMAT.sh
+-rw-r--r--+ 1 mascenci nobody  142 Aug 10  2022 dune_setup.sh
+-rw-r--r--+ 1 mascenci nobody 1166 Oct 27  2022 RunANNIEsetup.sh
+drwxrwxr-x+ 2 mascenci nobody 2048 Nov 11  2022 beam_monitoring
+-rw-r--r--+ 1 mascenci nobody 1208 Oct 29  2023 tmux_marvin.sh
+-rw-r--r--+ 1 mascenci nobody   54 Jan 25  2024 tmux-client-15381.log
+-rw-r--r--+ 1 mascenci nobody 1947 Jan 26  2024 tmux_test.sh
+-rw-r--r--+ 1 mascenci nobody   54 Mar 18  2024 tmux-client-6116.log
+-rw-r--r--+ 1 mascenci nobody 5548 May 16 16:31 RunANNIEsetup2.sh
+-rw-r--r--+ 1 mascenci nobody  169 Jun 30 22:47 acdc.sh
+[mascenci@anniegpvm01 ~]$ app
+/exp/annie/app/users/mascenci/
+[mascenci@anniegpvm01 mascenci]$ cd ToolAnalysis_Yue
+[mascenci@anniegpvm01 ToolAnalysis_Yue]$ ls -lrt
+total 444364
+lrwxrwxrwx   1 mascenci annie        41 Aug 15 20:49  BeamTimeStudy -> configfiles/BeamTimeStudy/ToolChainConfig
+-rwxr-xr-x   1 mascenci annie        79 Aug 15 20:49  CI.sh
+-rw-r--r--   1 mascenci annie      2869 Aug 15 20:49  CMakeLists.txt
+drwxr-xr-x  86 mascenci annie        86 Aug 15 20:51  configfiles
+lrwxrwxrwx   1 mascenci annie        42 Aug 15 20:52  DNNTrackLength -> configfiles/DNNTrackLength/ToolChainConfig
+-rw-r--r--   1 mascenci annie       345 Aug 15 20:52  Dockerfile
+-rw-r--r--   1 mascenci annie      2621 Aug 15 20:52  Dockerfile.base
+drwxr-xr-x   4 mascenci annie       724 Aug 15 20:53  docs
+lrwxrwxrwx   1 mascenci annie        38 Aug 15 20:54  EnergyReco -> configfiles/EnergyReco/ToolChainConfig
+-rw-r--r--   1 mascenci annie     99231 Aug 15 20:54  Doxyfile
+lrwxrwxrwx   1 mascenci annie        50 Aug 15 20:54  Example3 -> configfiles/Example3-LoadStoreData/ToolChainConfig
+lrwxrwxrwx   1 mascenci annie        53 Aug 15 20:54  Example2 -> configfiles/Example2-GenerateRootData/ToolChainConfig
+lrwxrwxrwx   1 mascenci annie        54 Aug 15 20:54  Example1 -> configfiles/Example1-GenerateStoreData/ToolChainConfig
+lrwxrwxrwx   1 mascenci annie        45 Aug 15 20:54  FilterLAPPDEvents -> configfiles/FilterLAPPDEvents/ToolChainConfig
+lrwxrwxrwx   1 mascenci annie        53 Aug 15 20:54  Example6 -> configfiles/Example6-ToolChainception/ToolChainConfig
+lrwxrwxrwx   1 mascenci annie        48 Aug 15 20:54  Example5 -> configfiles/Example5-PythonTools/ToolChainConfig
+lrwxrwxrwx   1 mascenci annie        49 Aug 15 20:54  Example4 -> configfiles/Example4-LoadRootData/ToolChainConfig
+lrwxrwxrwx   1 mascenci annie        41 Aug 15 20:54  FindMrdTracks -> configfiles/FindMrdTracks/ToolChainConfig
+-rw-r--r--   1 mascenci annie       913 Aug 15 20:54  FindTrackLengthInWater
+-rw-r--r--   1 mascenci annie     13487 Aug 15 20:54  GetToolDAQ.sh
+-rw-r--r--   1 mascenci annie     15646 Aug 15 20:54  histogram2d.png
+-rw-r--r--   1 mascenci annie     12070 Aug 15 20:54  histogram.png
+lrwxrwxrwx   1 mascenci annie        36 Aug 15 20:54  LAPPDana -> configfiles/LAPPDana/ToolChainConfig
+-rw-r--r--   1 mascenci annie   5189351 Aug 15 20:54  LAPPDPlots.pdf
+-rw-r--r--   1 mascenci annie   3143330 Aug 15 20:54  LAPPDPlots.root
+-rw-r--r--   1 mascenci annie       436 Aug 15 20:54  LAPPDThresRecoMRDinfo.txt
+-rw-r--r--   1 mascenci annie     17279 Aug 15 20:54  LAPPD_Time_Alignment_Hundred_Files_Board0_current.png
+-rw-r--r--   1 mascenci annie     24355 Aug 15 20:54  LAPPD_Time_Alignment_Hundred_Files_Board2_current.png
+-rw-r--r--   1 mascenci annie     12234 Aug 15 20:54  LAPPD_Time_Alignment_Thousand_Files_Board0_current.png
+-rw-r--r--   1 mascenci annie     12434 Aug 15 20:54  LAPPD_Time_Alignment_Thousand_Files_Board2_current.png
+-rw-r--r--   1 mascenci annie      1227 Aug 15 20:54  LICENSE.txt
+-rw-r--r--   1 mascenci annie        28 Aug 15 20:54  linkdefincludeheader.txt
+-rw-r--r--   1 mascenci annie        22 Aug 15 20:54  linkdefpostamble.txt
+-rw-r--r--   1 mascenci annie       140 Aug 15 20:54  linkdefpreamble.txt
+-rw-r--r--   1 mascenci annie     10950 Aug 15 20:54  Makefile
+-rw-r--r--   1 mascenci annie       664 Aug 15 20:54  mapPMTtoDAQCardChannel_phase1
+lrwxrwxrwx   1 mascenci annie        38 Aug 15 20:54  Monitoring -> configfiles/Monitoring/ToolChainConfig
+-rw-r--r--   1 mascenci annie      1562 Aug 15 20:54  mapPMTtoDAQCardChannel_test
+-rw-r--r--   1 mascenci annie       774 Aug 15 20:54  mon_run.py
+lrwxrwxrwx   1 mascenci annie        48 Aug 15 20:54  NeutSensitivityStudy -> configfiles/NeutSensitivityStudy/ToolChainConfig
+-rw-r--r--   1 mascenci annie     52752 Aug 15 20:54  offsetFit_MultipleLAPPD.cpp
+lrwxrwxrwx   1 mascenci annie        50 Aug 15 20:54  PhaseIIReco -> configfiles/VertexReco/PhaseIIReco/ToolChainConfig
+-rw-r--r--   1 mascenci annie       346 Aug 15 20:54  offset.txt
+lrwxrwxrwx   1 mascenci annie        57 Aug 15 20:54  PhaseIISanityCheck -> configfiles/VertexReco/PhaseIISanityCheck/ToolChainConfig
+-rw-r--r--   1 mascenci annie      4334 Aug 15 20:54  plot_histogram2.C
+-rw-r--r--   1 mascenci annie      4466 Aug 15 20:54  plot_histogram.C
+-rw-r--r--   1 mascenci annie    124869 Aug 15 20:54  ProcessedData_PMTMRDLAPPD_R4901S0p71
+-rw-r--r--   1 mascenci annie      2140 Aug 15 20:54  processRuns.sh
+-rw-r--r--   1 mascenci annie      1324 Aug 15 20:54  README.md
+-rwxr-xr-x   1 mascenci annie       694 Aug 15 20:54  run_pre_ana.sh
+drwxr-xr-x   2 mascenci annie         2 Aug 15 20:54  script
+-rwxr-xr-x   1 mascenci annie      1999 Aug 15 20:54  SetupFNAL.sh
+-rw-r--r--   1 mascenci annie      2757 Aug 15 20:54  Setup_python.sh
+-rw-r--r--   1 mascenci annie      1602 Aug 15 20:54  Setup.sh
+drwxr-xr-x   2 mascenci annie         2 Aug 15 20:54  src
+lrwxrwxrwx   1 mascenci annie        21 Aug 15 20:54  ToolDAQ -> /ToolAnalysis/ToolDAQ
+-rw-r--r--   1 mascenci annie    631564 Aug 15 20:54 'ToolDAQ doc.pdf'
+-rw-r--r--   1 mascenci annie    265127 Aug 15 20:54  TrigDebugBufferLeft.txt
+-rw-r--r--   1 mascenci annie         0 Aug 15 20:54  TrigDebug.txt
+-rw-r--r--   1 mascenci annie    167081 Aug 15 20:54  TrigDebugGrouped.txt
+-rw-r--r--   1 mascenci annie    235543 Aug 15 20:54  TrigDecoderDebug.txt
+lrwxrwxrwx   1 mascenci annie        42 Aug 15 21:19  EventBuilderV2 -> configfiles/EventBuilderV2/ToolChainConfig
+-rw-r--r--   1 mascenci annie    239632 Aug 15 21:24  mrd_events.root
+drwxr-xr-x   2 mascenci annie       104 Aug 15 23:51  DataModel
+drwxr-xr-x   2 mascenci annie       277 Aug 15 23:51  include
+drwxr-xr-x   2 mascenci annie        12 Aug 15 23:51  lib
+-rwxr-xr-x   1 mascenci annie    985712 Aug 15 23:52  Analyse
+drwxr-xr-x 183 mascenci annie       186 Aug 16 14:38  UserTools
+-rwxrwxrwx   1 mascenci annie      3539 Aug 21 13:00  FastMakeClean.sh
+-rw-r--r--   1 mascenci annie       479 Sep  4 17:54  MRD_2023_8_1_to_2024_7_5.root
+-rw-r--r--   1 mascenci annie 354553160 Sep  4 17:57  MRD_2023-8-1_to_2024-7-5.root
+-rw-r--r--   1 mascenci annie  27214155 Sep  4 18:03  MRD_2023-7-2_to_2024-7-19.root
+-rw-r--r--   1 mascenci annie  61951635 Sep  4 18:03  MRD_2023-7-2_to_2024-8-21.root
+[mascenci@anniegpvm01 ToolAnalysis_Yue]$ vi FastMakeClean.sh 
+
+#!/bin/bash
+# Dummy test by M. Ascencio, Aug-2024
+# mascenci@fnal.gov
+
+
+# Check if no arguments are passed
+if [ $# -eq 0 ]; then
+    echo "###################################################################################################"
+    echo "#  No arguments provided. Please provide at least one argument.                                   #"
+    echo "#  Example:  './FastMakeClea [Toolchain_name]'                                                    #"
+    echo "#  Use 'ln -s configfile/[toolchain_name]/ToolChainConfig [toolchain_name]' to generate the name. #"
+    echo "###################################################################################################"
+    exit 1
+fi
+
+# in case there is no symbolic
+
+echo "###################################################################################################"
+echo "Making the symbolic" 
+ln -s configfiles/$1/ToolChainConfig $1
+
+# default tools 
+darray=("recoANNIE" "PlotWaveforms" "Factory")
+farray=("PlotWaveforms")
+uarray=("Unity_recoANNIE" "PlotWaveforms" "Factory")
+
+
+# what files are you using?
+# --------------------------------------------
+# Store the elements into an array
+
+tooschain="configfiles/$1/ToolsConfig"
+array=($(grep -v '^#' $tooschain | awk '{print $2}'))
+echo " "
+echo "###################################################################################################"
+echo "Tools: "
+for element in "${array[@]}"; do
+        echo "tool --->>  " $element
+done
+echo "###################################################################################################"
+
+# --------------------------------------------
+# modify temporaly the cpp to zpp 
+cd UserTools
+find . -type f -name "*.zpp" -exec rename .zpp .cpp {} +
+find . -type f -name "*.cpp" -exec rename .cpp .zpp {} +
+
+# --------------------------------------------
+# Modify the Unity.h and the UserTools/Factory.cpp
+
+echo " "
+echo " "
+echo "Making Unity.h in UserTools/"
+echo "###################################################################################################"
+
+# rm old Unity.h
+rm Unity.h
+# Def tools
+for element in "${darray[@]}"; do
+        cd $element
+        find . -type f -name "*.zpp" -exec rename .zpp .cpp {} +
+        cd ..
+done
+
+for element in "${uarray[@]}"; do
+        echo "#include $element.h"
+        echo "#include \"$element.h\"" >> Unity.h
+done
+
+# My tools
+for element in "${array[@]}"; do
+        echo "#include $element.h"
+        cd $element
+        find . -type f -name "*.zpp" -exec rename .zpp .cpp {} +
+        cd ..
+        echo "#include \"$element.h\"" >> Unity.h
+done
+echo "###################################################################################################"
+
+# --------------------------------------------
+# Create the Factory.cpp file
+echo " "
+echo " "
+echo "Creating Factory.cpp in UserTools/Factory/"
+echo "###################################################################################################"
+cd Factory
+rm Factory.cpp
+cat <<EOF > Factory.cpp
+#include "Factory.h"
+
+Tool* Factory(std::string tool) {
+    Tool* ret = 0;
+EOF
+
+# Loop through the array and add the conditional checks
+for element in "${farray[@]}"; do
+    echo "    if (tool==\"$element\") ret=new $element;" >> Factory.cpp
+done
+
+# Loop through the array and add the conditional checks
+for element in "${array[@]}"; do
+    echo "    if (tool==\"$element\") ret=new $element;" >> Factory.cpp
+done
+
+# Close the function in the file
+cat <<EOF >> Factory.cpp
+
+    return ret;
+}
+EOF
+cat Factory.cpp
+echo "###################################################################################################"
+cd ../..
